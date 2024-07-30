88eaf5bb2514be06e4f7d858d8ecb28d9dec75dc
배 <slspqkqh@naver.com>
2024-07-30 09:27:55 +0900

    practice 1.0

---
+++ lambda_Test/src/main/java/com/lambda_Test/LambdaTestApplication.java
@@ -0,0 +1,16 @@
+package com.lambda_Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.lambda_Test.controller.lambdaController;
+
+@SpringBootApplication
+public class LambdaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdaTestApplication.class, args);
+		System.out.println("============================ START ============================");
+	}
+
+}
